a1f97ad5d62fcc0e3de6549f130896bcd7b7bb11
Carly <yaysunshine@gmail.com>
2012-09-13 00:58:58 -0500

    update to rails 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookOfSecrets::Application.config.secret_token = 'b0b25741a8fe1b004d05b07e755ca82d46938d090c9c84be15cb83e83f1286b642374d9397da9a77e9fd76fecc6da9b881120d90d46bd034e017f1e264fcfad7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BookOfSecrets::Application.config.session_store :cookie_store, :key => '_book-of-secrets_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BookOfSecrets::Application.config.session_store :active_record_store
